9e0555affe88175ae29bbdef1deb43436b58d902
rachels-archive <rachelhui@protonmail.com>
2024-09-27 23:25:47 +0800

    Load Roboto fonts into ffmpeg for video transcoding

--- next.config.mjs
+++ next.config.mjs
@@ -21,10 +21,8 @@ const nextConfig = {
   webpack(config) {
     config.module.rules.push({
       test: /\.(woff|woff2|eot|ttf|otf)$/,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
       type: "asset/resource",
-      generator: {
-        filename: "fonts/[name][hash][ext]",
-      },
     });
 
     return config;
--- src/components/ResultVideo.js
+++ src/components/ResultVideo.js
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState } from "react";
 import { FFmpeg } from "@ffmpeg/ffmpeg";
 import { fetchFile, toBlobURL } from "@ffmpeg/util";
 import { formatToSrt } from "@/libs/transcriptionHelpers";
+import roboto from "/public/fonts/Roboto-Regular.ttf";
+import robotoBold from "/public/fonts/Roboto-Bold.ttf";
 
 export default function ResultVideo({ videoUrl, fileName, transcriptionItems }) {
   const [loaded, setLoaded] = useState(false);
@@ -11,26 +13,50 @@ export default function ResultVideo({ videoUrl, fileName, transcriptionItems }) 
 
   useEffect(() => {
     videoRef.current.src = videoUrl;
-    load(); //load ffmpeg library
-  }, []);
+    load(); // Load ffmpeg library
+  }, [videoUrl]);
 
   const load = async () => {
     const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/umd";
     const ffmpeg = ffmpegRef.current;
+    console.log(roboto, robotoBold);
 
     await ffmpeg.load({
       coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
       wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, "application/wasm"),
     });
-    setLoaded(true);
+
+    await ffmpeg.writeFile("/tmp/roboto.ttf", await fetchFile(roboto));
+    await ffmpeg.writeFile("/tmp/roboto-bold.ttf", await fetchFile(robotoBold));
+    setLoaded(true); // Set loaded state to true
   };
 
   const transcode = async () => {
-    const ffmpeg = ffmpegRef.current;
-    await ffmpeg.writeFile(fileName, await fetchFile(videoUrl));
+    if (!loaded) {
+      console.error("FFmpeg is not loaded yet.");
+      return; // Prevent execution if FFmpeg is not loaded
+    }
 
-    console.log(formatToSrt(transcriptionItems));
-    await ffmpeg.exec(["-i", fileName, "output.mp4"]);
+    const ffmpeg = ffmpegRef.current;
+    const srt = formatToSrt(transcriptionItems);
+
+    await ffmpeg.writeFile(fileName, await fetchFile(videoUrl));
+    await ffmpeg.writeFile("subs.srt", srt);
+
+    ffmpeg.on("log", ({ message }) => {
+      console.log(message);
+    });
+
+    await ffmpeg.exec([
+      "-i",
+      fileName,
+      "-preset",
+      "ultrafast",
+      "-vf",
+      `subtitles=subs.srt:fontsdir=/tmp:force_style='Fontname=Roboto,FontSize=30,MarginV=70'`,
+      "output.mp4",
+    ]);
+
     const data = await ffmpeg.readFile("output.mp4");
     videoRef.current.src = URL.createObjectURL(new Blob([data.buffer], { type: "video/mp4" }));
   };
@@ -41,6 +67,7 @@ export default function ResultVideo({ videoUrl, fileName, transcriptionItems }) 
         <button
           className="bg-orange-500 py-3 px-6 rounded-full inline-flex gap-2 hover:bg-orange-600 cursor-pointer"
           onClick={transcode}
+          disabled={!loaded} // Disable button if not loaded
         >
           <SparklesIcon />
           <span>Apply captions</span>
